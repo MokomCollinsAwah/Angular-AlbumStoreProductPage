efcaa894d926820de63373ea6b3feb67e153cb35
Mokom Collins Awah <m.collinsawah@gmail.com>
2019-10-24 21:06:00 +0100

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,9 +7,10 @@ import { ProductService} from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  albumInfo;
+
   constructor(private _productService: ProductService) { }
 
-  albumInfo;
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
